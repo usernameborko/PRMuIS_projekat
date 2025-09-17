4944adc01aad1bd4c73b35e9193cb8de6188cd1c
boba <slobodan.tucakovic2@gmail.com>
2025-09-17 13:21:17 +0200

    Udp-prijava igraca

--- PotapanjePodmornica/Klijent/Klijent.cs
+++ PotapanjePodmornica/Klijent/Klijent.cs
@@ -5,6 +5,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("=== KLIJENT ZA IGRU 'POTAPANJE PODMORNICA' ===");
+
+            // kreiranje UDP socketa za prijavu
+            Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            // IP adresa servera
+            IPEndPoint serverUdp = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 15000);
+
+            Console.WriteLine("Pritisnite Enter da posaljete prijavu serveru...");
+            Console.ReadKey();
+
+            string prijava = "PRIJAVA";
+            byte[] prijavaBytes = Encoding.UTF8.GetBytes(prijava);
+
+            udpSocket.SendTo(prijavaBytes, serverUdp);
+            Console.WriteLine($"Prijava poslata serveru na {serverUdp}");
+
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] prijemniBafer = new byte[1024];
+            int brBajta = udpSocket.ReceiveFrom(prijemniBafer, ref remoteEP);
+
+            string odgovor = Encoding.UTF8.GetString(prijemniBafer, 0, brBajta);
+            Console.WriteLine($"[UDP] Odgovor servera: {odgovor}");
+            udpSocket.Close();
         }
     }
 }
--- PotapanjePodmornica/Server/Server.cs
+++ PotapanjePodmornica/Server/Server.cs
@@ -5,6 +5,46 @@
         static void Main(string[] args)
         {
             Console.WriteLine("=== SERVER ZA IGRU 'POTAPANJE PODMORNICA' ==='");
+
+            // unos parametara zadatak 2
+            Console.Write("Unesite broj igraca: ");
+            int brojIgraca = int.Parse(Console.ReadLine());
+
+            Console.Write("Unesite dimenziju table (npr. 5 za 5x5): ");
+            int dimenzija = int.Parse(Console.ReadLine());
+
+            Console.Write("Unesite broj dozvoljenih promasaja: ");
+            int promasaji = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nServer postavljen: igraci={brojIgraca}, tabla={dimenzija}x{dimenzija}, dozvoljeni promasaji={promasaji}");
+
+            // kreiranje UDP soketa za prijave
+            Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint udpEP = new IPEndPoint(IPAddress.Any, 15000);
+            udpSocket.Bind(udpEP);
+
+            Console.WriteLine($"[UDP] Server ceka prijave igraca na {udpEP}");
+
+            //cuvanje prijava
+            List<EndPoint> prijavljeni = new List<EndPoint>();
+
+            while (prijavljeni.Count < brojIgraca)
+            {
+                byte[] prijemniBafer = new byte[1024];
+                EndPoint posiljaocEP = new IPEndPoint(IPAddress.Any, 0);
+                int brBajta = udpSocket.ReceiveFrom(prijemniBafer, ref posiljaocEP);
+
+                string poruka = Encoding.UTF8.GetString(prijemniBafer, 0, brBajta);
+                Console.WriteLine($"[UDP] Primljena prijava: '{poruka}' od {posiljaocEP}");
+
+                if (poruka == "PRIJAVA" && !prijavljeni.Contains(posiljaocEP))
+                {
+                    prijavljeni.Add(posiljaocEP);
+                    Console.WriteLine($"[UDP] Igrac {prijavljeni.Count}/{brojIgraca} prijavljen.");
+                }
+            }
+
+            Console.WriteLine("\n=== Svi igraci su prijavljeni! ===");
         }
     }
 }
